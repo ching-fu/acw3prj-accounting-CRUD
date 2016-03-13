2e58c22360f5604bb54bf3a735734f49d82ba24d
ching-fu <gavaniyu@hotmail.com>
2016-03-13 22:42:48 +0800

    1. basic Read/Update 	2. add more options for each item

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,5 +1,6 @@
 class RecordsController < ApplicationController
 	def welcome
+		#log in page
 	end
 	def index
 		@records=Record.all
@@ -13,22 +14,27 @@ class RecordsController < ApplicationController
 		redirect_to records_path
 	end
 	def destroy
-		puts "---------------------------------"
 		puts params
 		@record=Record.find(params[:id])
 		@record.destroy
 		redirect_to records_path
 	end
 	def show
+		@record = Record.find(params[:id])
 	end
 	def edit
+		@record = Record.find(params[:id])
 	end	
-
+	def update
+		@record = Record.find(params[:id])
+		@record.update(record_params)		
+		redirect_to records_path		
+	end
 	private
 	#for Strong Parameters
 	#to read :name & :detail from params[:record]
 	def record_params
-  		params.require(:record).permit(:name, :detail, :date, :pay_way, :money, :type)
+  		params.require(:record).permit(:name, :detail, :date, :pay_way, :money, :group, :is_income)
 	end	
 
 end
